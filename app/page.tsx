8e2807282b8bd70086c9b7a343f6a9f1c1b91f28
404Nikhil <nicksd111@gmail.com>
2024-02-23 12:13:41 +0530

    auth buttton setup fiux

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Drop Files</h1>
+     <h1>DocDrop</h1>
      <UserButton />
     </main>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import { UserButton, SignInButton, SignedOut  } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-betweem">
-    <Link href="/">
-      <h1 className="font text-xl">Drop File</h1>
+    <header className='flex items-center justify-between'>
+    <Link className="flex items-center space-x-2" href="/">
+        <div>
+            <Image src="/docdrop.jpeg" alt="DocDrop Logo" width={30} height={30} />
+        </div>
+      <h1 className="font-bold text-xl">DocDrop</h1>
       </Link>
+
+      <div className='px-5 flex space-x-2 items-center'>
+        <UserButton afterSignOutUrl="/"/>
+        <SignedOut>
+            <SignInButton afterSignInUrl="/dashboard" mode="modal"/>
+        </SignedOut>
+      </div>
     </header>
   )
 }
