2c864cd8f0fa4feec86813e4dbd4e5f87af45458
404Nikhil <nicksd111@gmail.com>
2024-02-24 21:24:35 +0530

    Revert "db setup - onDrop interfacing"
    This reverts commit dd20e86da2c2d4f9f066992a630ef4cfc8bd1ddb.

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -1,39 +1,15 @@
 "use client";
-import { cn } from "@/lib/utils";
+
 import DropzoneComponent from "react-dropzone";
-import { useUser } from "@clerk/nextjs";
-import { useState } from "react";
 
 function Dropzone() {
-    const [loading, setLoading] = useState(false);
-    const {isLoaded, isSignedIn, user} = useUser();
-    const onDrop = (acceptedFiles: File[])=> {
-        acceptedFiles.forEach((file) => {
-                const reader = new FileReader();
-                reader.onabort = () => console.log("file reading was aborted");
-                reader.onerror = () => console.log("file reading has failed");
-                reader.onload = async () => {
-                    await uploadPost(file);
-            };
-            reader.readAsArrayBuffer(file);
-
-        });
-    };
-        const maxSize = 20971520;
-        const uploadPost = async(selectedFile: File) => {
-            if(loading) return;
-            if(!user) return;
-            setLoading(true);
-
-            setLoading(false); 
-
-        }
+    const maxSize = 20971520;
 
     return (
         <DropzoneComponent 
             minSize={0} 
             maxSize={maxSize} 
-            onDrop={onDrop}>
+            onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
             {({ getRootProps,
                getInputProps, 
                isDragActive, 
@@ -42,11 +18,8 @@ function Dropzone() {
             }) => {
                 const isFileTooLarge = fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
                 return (
-                    <section className="m-8">
-                        <div {...getRootProps()}
-                        className=
-                            "w-full h-52 p-5 border-dashed border-2 border-white rounded-lg flex justify-center items-center cursor-pointer"
-                        >
+                    <section className="flex justify-center items-center h-screen border-white bg-red-600 text-white">
+                        <div {...getRootProps()}>
                             <input {...getInputProps()} className="text-white"/>
                             {!isDragActive && "Click here or drop a file to upload!"}
                             {isDragActive && !isDragReject && "Drop it like it's hot!"}
